bda9372ec68abd68c720db262550321973322095
Tarun S G <tarunsg18@gmail.com>
2024-10-26 10:06:47 +0530

    .

--- login/mysql.sql
+++
@@ -1,9 +0,0 @@
-CREATE DATABASE mydatabase;
-USE mydatabase;
-
-CREATE TABLE users (
-    id INT AUTO_INCREMENT PRIMARY KEY,
-    uname VARCHAR(50) NOT NULL,
-    uemail VARCHAR(50) NOT NULL,
-    pword VARCHAR(50) NOT NULL
-);
